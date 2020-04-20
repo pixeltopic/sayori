4cf8d010897000b96221c4fe56765120d75bfd8a
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2020-04-19 21:37:17 -0700

    coalesce code for middleware example

--- examples/v1/middleware/README.md
+++ examples/v1/middleware/README.md
@@ -1,8 +1,8 @@
-# example: middlewares
+# example: middleware
 
-`middlewares` is a basic Discord bot that has a simple `AdminMiddleware` middleware defined.
+`middleware` is a basic Discord bot that has a simple `Checker` middleware defined.
 
-This middleware will be run when the command `.which` is invoked, and will determine whether the user calling it has admin privileges.
+This middleware will be run when the command `.p` is invoked, and will determine whether the user calling it has admin privileges.
 
 ## run
 
--- examples/v1/middleware/main.go
+++ examples/v1/middleware/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -16,9 +18,70 @@ var (
 	Token string
 )
 
+const defaultPrefix = "."
+
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
+}
+
+// Prefix loads a prefix
+type Prefix struct{}
+
+// Load returns the Default prefix no matter what the guildID given is.
+func (p *Prefix) Load(_ string) (string, bool) { return p.Default(), true }
+
+// Default returns the default router prefix
+func (*Prefix) Default() string { return defaultPrefix }
+
+// Checker checks if a user has admin privilege
+type Checker struct{}
+
+// Do a check for valid permissions
+func (*Checker) Do(ctx sayori.Context) error {
+	aperm, err := ctx.Session.State.UserChannelPermissions(
+		ctx.Message.Author.ID, ctx.Message.ChannelID)
+	if err != nil {
+		return err
+	}
+
+	if aperm&discordgo.PermissionAdministrator != discordgo.PermissionAdministrator {
+		return errors.New("you don't have admin perms :(")
+	}
+
+	return nil
+}
+
+// Privilege is a privileged command only admins can use.
+type Privilege struct{}
+
+// Match examines the first token to see if it matches a valid alias
+func (*Privilege) Match(toks sayori.Toks) (string, bool) {
+	alias, ok := toks.Get(0)
+	if !ok {
+		return "", false
+	}
+	alias = strings.ToLower(alias)
+
+	for _, validAlias := range []string{"p", "priv", "privileged"} {
+		if alias == validAlias {
+			return alias, true
+		}
+	}
+	return "", false
+}
+
+// Handle handles the echo command
+func (*Privilege) Handle(ctx sayori.Context) error {
+	_, _ = ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, "You are privileged!")
+	return nil
+}
+
+// Resolve handles any errors
+func (*Privilege) Resolve(ctx sayori.Context) {
+	if ctx.Err != nil {
+		_, _ = ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, ctx.Err.Error())
+	}
 }
 
 func main() {
@@ -28,9 +91,9 @@ func main() {
 		return
 	}
 
-	router := sayori.New(dg, &Prefixer{})
+	router := sayori.New(dg, &Prefix{})
 
-	router.Has(router.Command(&AdminCmd{}).Use(&AdminMiddleware{}))
+	router.Has(router.Command(&Privilege{}).Use(&Checker{}))
 
 	err = router.Open()
 	if err != nil {
